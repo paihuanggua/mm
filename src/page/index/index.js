f28be4faa938dbf401b9fc95eccb1c671d058659
zgz <421677055@qq.com>
2017-08-27 13:47:44 +0800

    initial

---
+++ src/page/index/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: zgz
+ * @Date:   2017-08-27 10:46:07
+ * @Last Modified by:   zgz
+ * @Last Modified time: 2017-08-27 11:54:28
+ */
+
+console.log('page index');
+
+require('./index.css');
+require('../module.js');
+// var $ = require('jquery');
+// $('body').html('hello index page ~~~~~~111')
---
+++ src/page/login/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: zgz
+ * @Date:   2017-08-27 10:45:07
+ * @Last Modified by:   zgz
+ * @Last Modified time: 2017-08-27 11:33:10
+ */
+
+
+console.log('page login');
+require('../module.js');
